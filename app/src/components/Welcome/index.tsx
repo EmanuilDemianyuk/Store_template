e48b7534a75df39725c82d5e3b87227bcef8ebc0
EmanuilDemianyuk <120477589+EmanuilDemianyuk@users.noreply.github.com>
2023-10-01 17:42:56 +0300

    work on Welcome com

--- app/src/components/Welcome/index.tsx
+++ app/src/components/Welcome/index.tsx
@@ -5,10 +5,16 @@ import ponehome from '../../assets/img/ponehome.png';
 import fries from '../../assets/img/frieshome.png';
 import ptwohome from '../../assets/img/ptwohome.png';
 
+// type Styles = {
+//     backgroundItem: string
+// }
+
 function Welcome():JSX.Element {
+    // const stylesToProducts: Styles = {
+    //     backgroundItem: '',
+    // }
     return (
-        <section className='container rounded bg-green-700 mb-5 max-h-max max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4'>
-            <div className='flex text-white'>
+        <section className='container flex text-white rounded bg-green-700 mb-5 max-h-max max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4'>
                 <div className='mt-10 mb-10 sm:basis-1/2 md:basis-2/5 md:mt-24 md:mb-24 lg:basis-1/2 lg:mt-24 lg:mb-24'>
                     <h2 className='font-header text-3xl lg:text-5xl mb-3'>
                         Tasty Meals made for You
@@ -20,29 +26,50 @@ function Welcome():JSX.Element {
                         View Menu
                     </Button>
                 </div>
-                <div className='hidden sm:block sm:basis-3/5 lg:basis-1/2'>
-                    <div className='relative'>
+                {/* <div className='hidden sm:block sm:basis-3/5 lg:basis-1/2 relative'>
+
                         <div className='hidden md:block md:absolute md:w-32 md:h-32 md:top-10 md:left-10 lg:left-20 rounded-full bg-light-green'>
                             <img className='w-24 ml-2 mt-2' src={lemonade} alt='lemonade'/>
                         </div>
-                    </div>
-                    <div className='relative'>
+
                         <div className='absolute top-10 -left-20 w-80 sm:w-72 sm:h-72 sm:top-10 sm:left-12 md:w-56 md:h-56 md:top-0 md:left-60 lg:left-72 rounded-full bg-light-green'>
-                            <img className='md:w-52 md:ml-2 md:mt-2' src={ponehome} alt='pizza'/>
+                            <img className=' md:w-52 md:ml-2 md:mt-2' src={ponehome} alt='pizza'/>
                         </div>
-                    </div>
-                    <div className='relative'>
+                    
                         <div className='hidden md:block md:absolute md:w-40 md:h-40 md:top-44 md:left-20 lg:left-32 rounded-full bg-light-green'>
                             <img className='w-52 mt-8' src={fries} alt='pizza'/>
                         </div>
-                    </div>
-                    <div className='relative'>
+
                         <div className='hidden md:block md:absolute md:w-52 md:h-52 md:top-64 md:right-0 lg:-right-5 lg:top-60 rounded-full bg-light-green'>
                             <img className='w-48 ml-2 mt-2' src={ptwohome} alt='pizza'/>
                         </div>
-                    </div>
+                </div> */}
+                <div className='flex relative basis-1/2 w-full flex-wrap md:basis-3/5'>
+
+                        <div className='flex basis-1/2 justify-center items-center'>
+                            <div className='p-2 bg-BrandWrapperGB rounded-full overflow-hidden'>
+                                <img className='w-full h-full object-fill' src={lemonade} alt='lemonade'/>
+                            </div>
+                        </div>
+
+                        <div className='flex basis-1/2'>
+                            <div className='p-2 bg-BrandWrapperGB rounded-full'>
+                                <img className='w-full h-full object-fill' src={ponehome} alt='pizza'/>
+                            </div>
+                        </div>
+                    
+                        <div className='flex basis-1/2 items-center'>
+                            <div className='p-2 bg-BrandWrapperGB rounded-full overflow-hidden '>
+                                <img className='w-full h-full object-fill' src={fries} alt='pizza'/>
+                            </div>
+                        </div>
+
+                        <div className='flex basis-1/2'>
+                            <div className='p-2 bg-BrandWrapperGB rounded-full max-w-52 max-h-52'>
+                                <img className='w-full h-full object-fill' src={ptwohome} alt='pizza'/>
+                            </div>
+                        </div>
                 </div>
-            </div>
         </section>
     );
 }
