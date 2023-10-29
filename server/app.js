bbcf289a1fc43a94f764124942abe02a30f2cca9
EmanuilDemianyuk <120477589+EmanuilDemianyuk@users.noreply.github.com>
2023-10-29 22:00:56 +0200

    fix env file

--- server/app.js
+++ server/app.js
@@ -8,8 +8,6 @@ const productRouters = require('./routes/product-routers');
 const imageRouters = require('./routes/image-routers');
 const userRouters = require('./routes/user-routers');
 
-const URL__T0__DB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f6hpu31.mongodb.net/Store_Template`;
-
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -23,7 +21,7 @@ app.use(imageRouters);
 app.use(userRouters);
 
 mongoose
-  .connect(URL__T0__DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.URL_KEY_PASS_MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(`DB connection error: ${err}`));
 
