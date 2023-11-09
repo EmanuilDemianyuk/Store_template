628b94f9919705c5be989bbcf07ea51fc1620cc4
Arina Zinievych <120561883+arina-zinievych@users.noreply.github.com>
2023-11-09 11:32:51 +0200

    create SignUp component and styled

--- app/src/components/SignUp/index.tsx
+++ app/src/components/SignUp/index.tsx
@@ -1,23 +1,191 @@
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button, Input } from '@material-tailwind/react';
+import { useForm } from 'react-hook-form';
+import { useSetUserMutation } from '../../store/api/users.api';
 import { IUser } from '../../typesOrInterface/interface';
+import styles from './style.module.scss';
 
 const SignUp = () => {
+  const { register, reset, handleSubmit, formState: { errors } } = useForm<IUser>();
+  const [setUser] = useSetUserMutation();
 
-  const { register, resetField, handleSubmit, formState: { errors } } = useForm<IUser>();
+  const onSubmit = async (dataForm: IUser) => {
+    try {
+      // const payload = await setUser(dataForm).unwrap();
+      // const userData = JSON.stringify(payload);
 
-  const onSubmit: SubmitHandler<IUser> = data => {
-
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div>
+    <section className={styles.SignUp}>
+      <h3>Sign Up</h3>
+      <p className={styles.SignUp__subtitle}>Nice to meet you! Enter your details to register.</p>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('id')} type='text'/>
-        
+        <div className={styles.SignUp__block}>
+          <div>
+            <Input 
+              {...register('userName', {
+                required: 'Name is required',
+                minLength: 3
+              })}
+              size="lg"
+              color="green"
+              name='userName'
+              label="Name*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.userName?.message}</p>
+          </div>
+          <div>
+            <Input 
+             {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: 'Invalid email format'
+              }
+              })}
+              label="Email*" 
+              size="lg" 
+              crossOrigin={undefined} 
+              type="email"
+              color="green"
+              name='email'
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.email?.message}</p>
+          </div>
+          <div>
+            <Input 
+              {...register('phone', {
+                  required: 'Mobile number is required',
+                  pattern: {
+                      value: /^\+380\d{9}$/,
+                      message: 'Mobile number must start with +380 and 9 more digits'
+                  }
+              })}
+              type='text'
+              size="lg"
+              color="green"
+              name='phone'
+              label="Mobile number*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.phone?.message}</p>
+          </div> 
+        </div>
+        <div className={styles.SignUp__block}>
+          <div>
+            <Input 
+              {...register('address.address.city')}
+              value="Harrisburg"
+              size="lg"
+              color="green"
+              name="city"
+              label="Harrisburg"
+              crossOrigin={undefined} 
+              disabled={true}
+            />
+          </div>
+          <div>
+            <Input 
+              {...register('address.address.street', {
+                  required: 'Street is required'
+              })}
+              size="lg"
+              color="green"
+              name='street'
+              label="Street*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.address?.address?.street?.message}</p>
+          </div>
+          <div>
+            <Input 
+              {...register('address.address.number', {
+                  required: 'House building is required'
+              })}
+              size="lg"
+              color="green"
+              name='house'
+              label="House building*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.address?.address?.number?.message}</p>
+          </div>
+        </div>
+        <div className={styles.SignUp__block}>
+          <div>
+            <Input 
+              {...register('address.address.entrance', {
+                required: 'Entrance is required'
+              })}
+              size="lg"
+              color="green"
+              name='entrance'
+              label="Entrance*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.address?.address?.entrance?.message}</p>
+          </div>
+          <div>
+            <Input 
+              {...register('address.address.floor', {
+                required: 'Floor is required'
+              })}
+              size="lg"
+              color="green"
+              name='floor'
+              label="Floor*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.address?.address?.floor?.message}</p>
+          </div>
+          <div>
+            <Input 
+              {...register('address.address.apartment', {
+                required: 'Apartment is required'
+              })}
+              size="lg"
+              color="green"
+              name='apartment'
+              label="Apartment*"
+              crossOrigin={undefined} 
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.address?.address?.apartment?.message}</p>
+          </div>
+        </div>
+        <div className={styles.SignUp__block}>
+          <div>
+            <Input 
+              {...register('password', {
+              required: 'Password is required',
+              minLength: 5
+              })}
+              label="Password*" 
+              size="lg" 
+              color="green"
+              crossOrigin={undefined} 
+              type="password"
+              name='password'
+            />
+            <p className={styles.SignUp__errorMsg}>{errors.password?.message}</p>
+          </div>
+        </div>
+        <div className={styles.SignUp__blockBtn}>
+          <Button 
+            variant="filled" 
+            type="submit"
+            >
+              Sign Up
+          </Button>
+        </div>
       </form>
-    </div>
-  )
+    </section>
+  );
 }
 
-export default SignUp
+export default SignUp;
