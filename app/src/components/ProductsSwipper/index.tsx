58063146b980c8f77d281771540fd890046b6c6f
EmanuilDemianyuk <120477589+EmanuilDemianyuk@users.noreply.github.com>
2024-08-19 20:27:55 +0300

    (fix): bug with swiper module has been removed, swiper module has been upgraded to a newer version

--- app/src/components/ProductsSwipper/index.tsx
+++ app/src/components/ProductsSwipper/index.tsx
@@ -6,7 +6,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 
-function ProductsSwipper (props: ProductsSwipperProps) {
+function ProductsSwipper(props: ProductsSwipperProps) {
     const styleSwiper: { [key: string]: string } = { "--swiper-navigation-color": "#068C52" };
 
     const { products } = props;
@@ -18,19 +18,19 @@ function ProductsSwipper (props: ProductsSwipperProps) {
             slidesPerView={3}
             navigation
             breakpoints={{
-                1700:{
+                1700: {
                     slidesPerView: 3,
                     spaceBetween: 50
                 },
-                1500:{
+                1500: {
                     slidesPerView: 3,
                     spaceBetween: 50
                 },
-                915:{
+                915: {
                     slidesPerView: 3,
                     spaceBetween: 30
                 },
-                865:{
+                865: {
                     slidesPerView: 2,
                     spaceBetween: 30
                 },
@@ -38,7 +38,7 @@ function ProductsSwipper (props: ProductsSwipperProps) {
                     slidesPerView: 2,
                     spaceBetween: 20
                 },
-                400:{
+                400: {
                     slidesPerView: 1,
                     spaceBetween: 10
                 },
@@ -47,21 +47,21 @@ function ProductsSwipper (props: ProductsSwipperProps) {
                     spaceBetween: 0
                 }
             }}
-            >
+        >
             {
-            products?.map(card => (
-            <SwiperSlide key={card.id}>
-                <ProductCard 
-                    id={card.id}
-                    name={card.name} 
-                    description={card.description} 
-                    price={card.price} 
-                    img={card.img} 
-                    category={card.category}
-                    type={card.type}
-                    rating={card.rating} />
-            </SwiperSlide>
-            ))
+                products?.map(card => (
+                    <SwiperSlide key={card.id}>
+                        <ProductCard
+                            id={card.id}
+                            name={card.name}
+                            description={card.description}
+                            price={card.price}
+                            img={card.img}
+                            category={card.category}
+                            type={card.type}
+                            rating={card.rating} />
+                    </SwiperSlide>
+                ))
             }
         </Swiper>
     );
